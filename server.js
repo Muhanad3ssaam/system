da688352109c7bd95b21596e9f9176acce010939
Glitch (hello-express) <none>
2020-05-27 07:25:51 +0000

    🌾👠 Checkpoint ./server.js:13770949/1503

--- server.js
+++ server.js
@@ -2599,3 +2599,41 @@ client.on("message", async message => {
 - [       All Copy Right Reserved For: MoJRemGames/MG      ] -
 
 */
+
+const { stripIndents } = require("common-tags");
+client.on('message', message => {
+if (message.author.bot) return;
+if (message.content.startsWith(prefix+"report")) {
+let args = message.content.split(" ").slice(1)
+ let rMember = message.mentions.members.first() || message.guild.members.get(args[0]);
+
+        if (!rMember)
+            return message.reply("Couldn't find that person?").then(m => m.delete(5000));
+
+        if (rMember.hasPermission("BAN_MEMBERS") || rMember.user.bot)
+            return message.channel.send("Can't report that member").then(m => m.delete(5000));
+
+        if (!args[1])
+            return message.channel.send("Please provide a reason for the report").then(m => m.delete(5000));
+        
+        const channel = message.guild.channels.find(c => c.name === "reports")
+            
+        if (!channel)
+            return message.channel.send("Couldn't find a `#reports` channel").then(m => m.delete(5000));
+
+        const embed = new RichEmbed()
+            .setColor("#ff0000")
+            .setTimestamp()
+            .setFooter(message.guild.name, message.guild.iconURL)
+            .setAuthor("Reported member", rMember.user.displayAvatarURL)
+            .setDescription(stripIndents`**- Member:** ${rMember} (${rMember.user.id})
+            **- Reported by:** ${message.member}
+            **- Reported in:** ${message.channel}
+            **- Reason:** ${args.slice(1).join(" ")}`);
+
+        return channel.send(embed);
+    }
+
+
+          
+});
